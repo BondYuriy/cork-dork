4250fe825a252cce7c8b9da07d2348167ec4ea50
Yuriy Bondar <bondar.europe@gmail.com>
2020-07-10 23:32:12 +0300

    adds new home slider

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -45,6 +45,23 @@ $(document).ready(function () {
     },
   });
 
+  $('.slider-for').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.slider-nav'
+  });
+  $('.slider-nav').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    asNavFor: '.slider-for',
+    dots: true,
+    centerMode: false,
+    focusOnSelect: true
+  });
+
+
   /* to-top */
 
   $('#toTop').click(function () {
